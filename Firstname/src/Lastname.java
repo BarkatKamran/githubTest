970d1dbe5bf77b55d0273f4cad3a2898159f719b
BarkatKamran <71576244+BarkatKamran@users.noreply.github.com>
2020-09-19 21:07:17 -0700

    Add files via upload

---
+++ Firstname/src/Lastname.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Lastname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String userFirstnam;
+		String userLastnam;
+		userFirstnam=JOptionPane.showInputDialog("Please Enter your First Name");
+		userLastnam=JOptionPane.showInputDialog("Please Enter your Last Name");
+		JOptionPane.showMessageDialog(null, userFirstnam+" "+userLastnam);
+
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Hi/src/Sum.java
@@ -0,0 +1,13 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b=21;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
